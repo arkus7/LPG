f4ca93dc790672dfe0c3b3814a452b80522e3e5f
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-03-18 19:59:28 +0100

    update base activity

--- app/src/main/java/com/zmudni/lpg/activities/BaseActivity.java
+++ app/src/main/java/com/zmudni/lpg/activities/BaseActivity.java
@@ -1,4 +1,30 @@
 package com.zmudni.lpg.activities;
 
-public class BaseActivity {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+
+import com.zmudni.lpg.fragments.BaseFragment;
+
+import butterknife.ButterKnife;
+
+public abstract class BaseActivity extends FragmentActivity {
+    protected abstract void init();
+    protected abstract int getLayout();
+    public abstract int getFragmentContainerId();
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayout());
+        ButterKnife.bind(this);
+    }
+
+    public void showFragment(BaseFragment fragment, String tag) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(getFragmentContainerId(), fragment, tag);
+        transaction.commitAllowingStateLoss();
+    }
 }
