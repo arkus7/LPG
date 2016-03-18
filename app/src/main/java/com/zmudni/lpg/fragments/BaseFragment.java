91269cca74979e08217ec25e7441376833e17408
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-03-18 19:59:40 +0100

    update base fragment

--- app/src/main/java/com/zmudni/lpg/fragments/BaseFragment.java
+++ app/src/main/java/com/zmudni/lpg/fragments/BaseFragment.java
@@ -1,4 +1,36 @@
 package com.zmudni.lpg.fragments;
 
-public class BaseFragment {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+public abstract class BaseFragment extends Fragment {
+    public abstract int getLayout();
+    public abstract void init();
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(getLayout(), container, false);
+        ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+
 }
