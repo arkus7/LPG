f461329788f5df67318aed16d8f72892b895aad3
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-03-18 20:32:04 +0100

    update catch color circles activity

--- app/src/main/java/com/zmudni/lpg/activities/CatchColorCirclesActivity.java
+++ app/src/main/java/com/zmudni/lpg/activities/CatchColorCirclesActivity.java
@@ -1,4 +1,22 @@
 package com.zmudni.lpg.activities;
 
-public class CatchColorCirclesActivity {
+import com.zmudni.lpg.R;
+import com.zmudni.lpg.fragments.CatchColorCirclesFragment;
+
+public class CatchColorCirclesActivity extends BaseActivity {
+
+    @Override
+    protected void init() {
+        showFragment(new CatchColorCirclesFragment(), "TAG");
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_catch_color_circles;
+    }
+
+    @Override
+    public int getFragmentContainerId() {
+        return R.id.catch_color_circles_fragment_container;
+    }
 }
