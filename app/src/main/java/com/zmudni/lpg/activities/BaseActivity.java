6733556f946e2eead873a8756bbdbc1a911415ae
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-03-19 00:36:45 +0100

    set window to fullscreen

--- app/src/main/java/com/zmudni/lpg/activities/BaseActivity.java
+++ app/src/main/java/com/zmudni/lpg/activities/BaseActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.zmudni.lpg.fragments.BaseFragment;
 
@@ -19,9 +21,16 @@ public abstract class BaseActivity extends FragmentActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setWindowToFullscreenMode();
         setContentView(getLayout());
         ButterKnife.bind(this);
         init();
+    }
+
+    private void setWindowToFullscreenMode() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
     public void showFragment(BaseFragment fragment, String tag) {
