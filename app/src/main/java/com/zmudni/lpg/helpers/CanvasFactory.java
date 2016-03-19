291a5c584ddf42d72c05221a387cd2d29a3a84a3
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-03-19 10:31:39 +0100

    refactor canvas factory, add scaling background image

--- app/src/main/java/com/zmudni/lpg/helpers/CanvasFactory.java
+++ app/src/main/java/com/zmudni/lpg/helpers/CanvasFactory.java
@@ -1,9 +1,6 @@
 package com.zmudni.lpg.helpers;
 
-import android.app.Activity;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.RectF;
@@ -13,16 +10,16 @@ import com.zmudni.lpg.Creature;
 import com.zmudni.lpg.Entity;
 import com.zmudni.lpg.Monster;
 import com.zmudni.lpg.Player;
-import com.zmudni.lpg.R;
 
 import java.util.List;
 
 public class CanvasFactory {
     public static Canvas canvas;
     private final int FONT_OFFSET_Y = 10;
-    private final int FONT_OFFSET_X = -10;
+    private final int FONT_OFFSET_X = - 10;
     private final int FONT_SIZE = 30;
-    private Entity  target;
+    private Entity target;
+
     public CanvasFactory(Canvas canvas) {
         this.canvas = canvas;
     }
@@ -33,17 +30,16 @@ public class CanvasFactory {
         return this;
     }
 
-    public CanvasFactory setText(String text, float x, float y,int size) {
+    public CanvasFactory setText(String text, float x, float y, int size) {
         Paint paint = new Paint();
         paint.setTextSize(FONT_SIZE);
-        paint.setARGB(255,255,255,255);
+        paint.setARGB(255, 255, 255, 255);
         canvas.drawText(text, x, y, paint);
         return this;
     }
 
-    public CanvasFactory setBackgroudImage(Bitmap bitmap) {
-        canvas.drawBitmap(bitmap, null, new RectF(0, 0, 1280, 720), null);
-        //canvas.drawBitmap(bitmap, 0, 0, null);
+    public CanvasFactory setBackgroudImage(Bitmap bitmap, int screenWidth, int screenHeight) {
+        canvas.drawBitmap(bitmap, null, new RectF(0, 0, screenHeight, screenWidth), null);
         return this;
     }
 
@@ -53,7 +49,7 @@ public class CanvasFactory {
     }
 
     public CanvasFactory drawCircleObject(CircleObject object) {
-        canvas.drawCircle(object.getX(), object.getY(), object.getRadius(),new Paint());
+        canvas.drawCircle(object.getX(), object.getY(), object.getRadius(), new Paint());
         return this;
     }
 
@@ -63,8 +59,8 @@ public class CanvasFactory {
         paint.setARGB(255, 255, 255, 255);
         canvas.drawBitmap(creature.getBitmap(), creature.getX(), creature.getY(), null);
         canvas.drawText("HP: " + creature.getHeathPoints() + "/" + creature.getMaxHealthPoints(), creature.getX(), creature.getY() + creature.getBitmap().getHeight() + FONT_OFFSET_Y + 25, paint);
-        canvas.drawText("Lvl: " + ((Player) creature).getCurrentLevel(), creature.getX() , creature.getY() + creature.getBitmap().getHeight() + FONT_OFFSET_Y + 50, paint);
-        canvas.drawText("Exp: " + ((Player)creature).getExperience()+"/"+((Player)creature).getNextLevelOn(), creature.getX(), creature.getY() + creature.getBitmap().getHeight() + FONT_OFFSET_Y+75, paint);
+        canvas.drawText("Lvl: " + ((Player) creature).getCurrentLevel(), creature.getX(), creature.getY() + creature.getBitmap().getHeight() + FONT_OFFSET_Y + 50, paint);
+        canvas.drawText("Exp: " + ((Player) creature).getExperience() + "/" + ((Player) creature).getNextLevelOn(), creature.getX(), creature.getY() + creature.getBitmap().getHeight() + FONT_OFFSET_Y + 75, paint);
         return this;
     }
 
@@ -73,16 +69,16 @@ public class CanvasFactory {
         int i = 0;
         Paint paint = new Paint();
         paint.setTextSize(FONT_SIZE);
-        paint.setARGB(255,255,255,255);
+        paint.setARGB(255, 255, 255, 255);
         for (Monster creature : creatures) {
             canvas.drawBitmap(creature.getBitmap(), creature.getX(), creature.getY(), null);
-            if (i == target){
-                entity.setX(creature.getX()-40);
-                entity.setY(creature.getY()+((creature.getBitmap().getHeight())/2)-30);
+            if (i == target) {
+                entity.setX(creature.getX() - 40);
+                entity.setY(creature.getY() + ((creature.getBitmap().getHeight()) / 2) - 30);
                 canvas.drawBitmap(entity.getBitmap(), entity.getX(), entity.getY(), null);
             }
-            canvas.drawText(creature.getWord().toLowerCase(), creature.getX()+ creature.getBitmap().getWidth()+10, creature.getY()+((creature.getBitmap().getHeight())/2), paint);
-            canvas.drawText("HP: " + creature.getHeathPoints() + "/" + creature.getMaxHealthPoints(), creature.getX()+50, creature.getY() + creature.getBitmap().getHeight() + FONT_OFFSET_Y-10, paint);
+            canvas.drawText(creature.getWord().toLowerCase(), creature.getX() + creature.getBitmap().getWidth() + 10, creature.getY() + ((creature.getBitmap().getHeight()) / 2), paint);
+            canvas.drawText("HP: " + creature.getHeathPoints() + "/" + creature.getMaxHealthPoints(), creature.getX() + 50, creature.getY() + creature.getBitmap().getHeight() + FONT_OFFSET_Y - 10, paint);
 
             i++;
         }
