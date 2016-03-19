b4e51246695513810459587ab7645fe0f90306f2
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-03-19 01:20:36 +0100

    add start screen fragment

---
+++ app/src/main/java/com/zmudni/lpg/fragments/StartScreenFragment.java
@@ -0,0 +1,13 @@
+package com.zmudni.lpg.fragments;
+
+public class StartScreenFragment extends BaseFragment {
+    @Override
+    public int getLayout() {
+        return 0;
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
