c2bd59ad2e9de8623f813555e66ea0d50190f168
Arkadiusz Żmudzin <arkadiusz.zmudzin@gmail.com>
2016-03-19 09:21:30 +0100

    add elements learn fragment

---
+++ app/src/main/java/com/zmudni/lpg/fragments/ElementsLearnFragment.java
@@ -0,0 +1,66 @@
+package com.zmudni.lpg.fragments;
+
+import android.view.View;
+import android.widget.Button;
+
+import com.zmudni.lpg.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import butterknife.Bind;
+import butterknife.OnClick;
+
+public class ElementsLearnFragment extends BaseFragment {
+
+    @Bind(R.id.next_part_button)
+    Button nextButton;
+
+    @OnClick(R.id.next_part_button)
+    public void onNextPartButtonClick() {
+        //todo: hadnle next part
+    }
+
+    private Timer timer;
+    private int delayTime = 0;
+    private final int MAX_WAIT_TIME = 6;
+
+    @Override
+    public int getLayout() {
+        return R.layout.fragment_element_learn;
+    }
+
+    @Override
+    public void init() {
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new NextPartTimerTask(), 100, 1000);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        timer.cancel();
+    }
+
+    private boolean isNextPartButtonShowed() {
+        return nextButton.getVisibility() == View.VISIBLE;
+    }
+
+    class NextPartTimerTask extends TimerTask {
+
+        @Override
+        public void run() {
+            if(! isNextPartButtonShowed())
+                delayTime++;
+            if(delayTime >= MAX_WAIT_TIME) {
+                delayTime = 0;
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        nextButton.setVisibility(View.VISIBLE);
+                    }
+                });
+            }
+        }
+    }
+}
